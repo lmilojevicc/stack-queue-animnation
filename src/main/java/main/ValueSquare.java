39b8420509acfd7b02fe360997563e4a20ddf873
Luka <milojevic.luka2002@gmail.com>
2024-04-30 02:02:11 +0200

    Added value and getters and setters

--- src/main/java/main/ValueSquare.java
+++ src/main/java/main/ValueSquare.java
@@ -9,13 +9,31 @@ import javafx.scene.shape.Rectangle;
 public class ValueSquare extends StackPane {
     private Rectangle rectangle;
     private Label label;
+    private String val;
 
     public ValueSquare(String value) {
+        this.val = value;
         rectangle = new Rectangle(50, 50, Color.TRANSPARENT);
         rectangle.setStroke(Color.BLACK); // Set the border color
         label = new Label(value);
         label.setTextFill(Color.BLACK); // Set the text color
         setAlignment(Pos.CENTER);
         getChildren().addAll(rectangle, label);
+    }
+
+    public Rectangle getRectangle() {
+        return rectangle;
+    }
+
+    public void setRectangle(Rectangle rectangle) {
+        this.rectangle = rectangle;
+    }
+
+    public String getVal() {
+        return val;
+    }
+
+    public void setVal(String val) {
+        this.val = val;
     }
 }
