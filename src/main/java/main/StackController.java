aa52f017724d033b8572480fac9f089f96fa1afa
Luka <milojevic.luka2002@gmail.com>
2024-04-30 02:02:37 +0200

    Implemented find for stack

--- src/main/java/main/StackController.java
+++ src/main/java/main/StackController.java
@@ -1,22 +1,24 @@
 package main;
 
-import javafx.animation.FadeTransition;
-import javafx.animation.ParallelTransition;
-import javafx.animation.TranslateTransition;
+import javafx.animation.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Stack;
+
 import main.ValueSquare;
 
 public class StackController {
@@ -67,6 +69,74 @@ public class StackController {
             ParallelTransition parallelTransition = new ParallelTransition(translateTransition, fadeTransition);
             parallelTransition.setOnFinished(e -> stackVBox.getChildren().remove(ValueSquare));
             parallelTransition.play();
+        }
+    }
+
+    @FXML
+    private void handleFindButton() {
+        String value = inputField.getText();
+        if (!stack.isEmpty()) {
+            SequentialTransition sequentialTransition = new SequentialTransition();
+            boolean itemFound = false;
+
+            while (!stack.isEmpty()) {
+                ValueSquare valueSquare = stack.peek();
+
+                if (Objects.equals(valueSquare.getVal(), value)) {
+                    PauseTransition pauseTransition = new PauseTransition(Duration.seconds(0.1));
+                    sequentialTransition.getChildren().add(pauseTransition);
+
+                    sequentialTransition.setOnFinished(event -> {
+                        valueSquare.getRectangle().setFill(Color.valueOf("F6D966FF"));
+
+                        PauseTransition restoreTransition = new PauseTransition(Duration.seconds(3));
+                        restoreTransition.setOnFinished(restoreEvent -> {
+                            valueSquare.getRectangle().setFill(Color.TRANSPARENT);
+                        });
+
+                        restoreTransition.play();
+                    });
+
+                    itemFound = true;
+                    break;
+                } else {
+                    stack.pop();
+                    PauseTransition pauseTransition = new PauseTransition(Duration.seconds(1));
+                    sequentialTransition.getChildren().add(pauseTransition);
+
+                    TranslateTransition translateTransition = new TranslateTransition(Duration.millis(500), valueSquare);
+                    translateTransition.setToY(-100);
+
+                    FadeTransition fadeTransition = new FadeTransition(Duration.millis(500), valueSquare);
+                    fadeTransition.setFromValue(1);
+                    fadeTransition.setToValue(0);
+
+                    ParallelTransition parallelTransition = new ParallelTransition(translateTransition, fadeTransition);
+                    parallelTransition.setOnFinished(e -> stackVBox.getChildren().remove(valueSquare));
+
+                    sequentialTransition.getChildren().add(parallelTransition);
+                }
+
+
+            }
+
+            sequentialTransition.play();
+
+            if (!itemFound) {
+                sequentialTransition.setOnFinished(event -> {
+                    Label messageLabel = new Label("Item not in queue!");
+                    messageLabel.setStyle("-fx-text-fill: red;");
+                    stackVBox.getChildren().add(messageLabel);
+
+                    PauseTransition clearMessageTransition = new PauseTransition(Duration.seconds(2));
+                    clearMessageTransition.setOnFinished(clearEvent -> {
+                        stackVBox.getChildren().remove(messageLabel);
+                    });
+
+                    clearMessageTransition.play();
+                });
+            }
+
         }
     }
 
